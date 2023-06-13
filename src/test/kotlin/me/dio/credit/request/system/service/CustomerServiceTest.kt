1bf7aceaa2a8f4230ce6126c5ada6ce1892f0d2d
ViniciusJMR <viniciusdejesus605@gmail.com>
2023-06-13 10:00:51 -0300

    Implementado teste customer findById

--- src/test/kotlin/me/dio/credit/request/system/service/CustomerServiceTest.kt
+++ src/test/kotlin/me/dio/credit/request/system/service/CustomerServiceTest.kt
@@ -8,6 +8,7 @@ import io.mockk.junit5.MockKExtension
 import io.mockk.verify
 import me.dio.credit.request.system.entity.Address
 import me.dio.credit.request.system.entity.Customer
+import me.dio.credit.request.system.exception.BusinessException
 import me.dio.credit.request.system.repository.CustomerRepository
 import me.dio.credit.request.system.service.impl.CustomerService
 import org.assertj.core.api.Assertions
@@ -15,26 +16,60 @@ import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.test.context.ActiveProfiles
 import java.math.BigDecimal
+import java.util.Optional
+import kotlin.random.Random
 
 @ActiveProfiles("test")
 @ExtendWith(MockKExtension::class)
 class CustomerServiceTest {
     @MockK
     lateinit var customerRepository: CustomerRepository
-    @InjectMockKs lateinit var customerService: CustomerService
+    @InjectMockKs
+    lateinit var customerService: CustomerService
 
     @Test
-    fun `should create customer`(){
+    fun `should create customer`() {
         //given
         val fakeCustomer = buildCustomer()
-        every {customerRepository.save(any())} returns fakeCustomer
+        every { customerRepository.save(any()) } returns fakeCustomer
         //when
         val actual: Customer = customerService.save(fakeCustomer)
 
         //then
         Assertions.assertThat(actual).isNotNull
         Assertions.assertThat(actual).isSameAs(fakeCustomer)
-        verify(exactly = 1){customerRepository.save(fakeCustomer)}
+        verify(exactly = 1) { customerRepository.save(fakeCustomer) }
+    }
+
+    @Test
+    fun `should find customer by id`() {
+        //given
+        val fakeId: Long = Random.nextLong()
+        val fakeCustomer: Customer = buildCustomer(id = fakeId)
+        every { customerRepository.findById(fakeId) } returns Optional.of(fakeCustomer)
+
+        //when
+        val actual: Customer = customerService.findById(fakeId)
+
+        //then
+        Assertions.assertThat(actual).isNotNull
+        Assertions.assertThat(actual).isExactlyInstanceOf(Customer::class.java)
+        Assertions.assertThat(actual).isSameAs(fakeCustomer)
+        verify(exactly = 1) { customerRepository.findById(fakeId) }
+    }
+
+    @Test
+    fun `should not find customer by invalid id throw BusinessException`() {
+        //given
+        val fakeId:Long = Random.nextLong()
+        every { customerRepository.findById(fakeId) } returns Optional.empty()
+        //when
+
+        //then
+        Assertions.assertThatExceptionOfType(BusinessException::class.java)
+            .isThrownBy { customerService.findById(fakeId) }
+            .withMessage("Id $fakeId not found")
+        verify(exactly = 1) { customerRepository.findById(fakeId) }
     }
 
     private fun buildCustomer(
